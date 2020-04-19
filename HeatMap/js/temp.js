173cb9fbfa1341fea88d6e1fe7b9c7ca7a6a9b71
Vishwas Meshram <vishwasmeshram0459@gmail.com>
2020-04-19 17:14:00 +0530

    added legend

--- HeatMap/js/temp.js
+++ HeatMap/js/temp.js
@@ -21,10 +21,8 @@ const heatMapSVG = d3
 const legendSVG = d3
   .select('#legend')
   .append('svg')
-  .attr('height', '100')
-  .attr('width', `${WIDTH}`)
-  .append('g') // group so that we can add both axis
-  .attr('transform', `translate(${margin.left}, ${margin.top})`); // add some margin to visualization
+  .attr('height', '200')
+  .attr('width', `${WIDTH}`);
 
 // label on x axis
 heatMapSVG
@@ -67,9 +65,9 @@ const mouseover = d => {
         new Date(2010, d.month - 1, 1)
       )}</span>
       </br>
-    <span>${(d.variance + baseTemperature).toFixed(1)}&#8451;</span>
+    <span>${(d.variance + baseTemperature).toFixed(2)}&#8451;</span>
     </br>
-    <span>${d.variance.toFixed(1)}&#8451;</span>
+    <span>${d.variance}&#8451;</span>
     `
     )
     .style('left', `${d3.event.clientX + 10}px`)
@@ -97,78 +95,16 @@ d3.json(URL)
     xScale.domain(d3.extent(yearDataset));
     const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
 
-    let tempVariance = dataset.map(d => d.variance + baseTemperature);
-    let tempDelta = (d3.max(tempVariance, d => d) / 10).toFixed(1);
-    let legendDomain = [];
-    for (let i = 0; i <= 10; i++) {
-      legendDomain.push(+(i * tempDelta).toFixed(1));
-    }
-
     const colorScale = d3
-      .scaleThreshold()
-      .domain(legendDomain)
-      // used https://www.color-hex.com/ to get below color pallets
-      .range([
-        '#f1c27d',
-        '#ffdbac',
-        '#e0ac69',
-        '#c68642',
-        '#8d5524',
-        '#b2d8d8',
-        '#66b2b2',
-        '#008080',
-        '#006666',
-        '#004c4c',
-        '#003333',
-      ]);
+      .scaleQuantize()
+      .domain([baseTemperature - 6.976, baseTemperature + 5.228])
+      .range(d3.schemeSet1);
 
-    const legendXScale = d3
-      .scaleLinear()
-      .domain(d3.extent(legendDomain))
-      .range([200, WIDTH - 200]);
-    console.log('legendDomain', legendDomain);
-    const legendAxis = d3
-      .axisBottom(legendXScale)
-      .tickSize(25)
-      // .ticks(8)
-      .tickValues([...legendDomain])
-      .tickFormat(d3.format('.1f'));
-
-    const temp = legendSVG.append('g').call(legendAxis);
-    // .attr('transform', `translate(0,0)`);
-
-    temp.select('.domain').remove();
-
-    temp
-      .selectAll('rects')
-      .data(
-        colorScale.range().map(clr => {
-          let invertClr = colorScale.invertExtent(clr);
-          if (invertClr[0] == null || undefined)
-            invertClr[0] = legendXScale.domain()[0];
-          if (invertClr[1] == null || undefined)
-            invertClr[1] = legendXScale.domain()[1];
-          return invertClr;
-        })
-      )
-      .enter()
-      .insert('rect', '.tick')
-      .attr('height', 18)
-      .attr('x', function (d) {
-        return legendXScale(d[0]);
-      })
-      .attr('width', function (d) {
-        return legendXScale(d[1]) - legendXScale(d[0]);
-      })
-      .attr('fill', function (d) {
-        return colorScale(d[0]);
-      })
-      .attr('stroke', 'black');
     // display
     heatMapSVG
       .append('g')
       .attr('id', 'x-axis')
-      .attr('transform', `translate(3,${HEIGHT})`)
+      .attr('transform', `translate(0,${HEIGHT})`)
       .call(xAxis);
 
     // Yscale
@@ -179,6 +115,8 @@ d3.json(URL)
     // console.log(d3.extent(timeDataset));
     yScale.domain(d3.extent(timeDataset));
     const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%B'));
+    // verify yScales
+    // console.log(yScale(new Date(2010, 11, 1)));
 
     // display
     heatMapSVG
@@ -186,11 +124,6 @@ d3.json(URL)
       .attr('id', 'y-axis')
       // .attr('transform', `translate(0,${HEIGHT})`)
       .call(yAxis);
-
-    // check NaN value
-    dataset.forEach(d => {
-      console.log('d.month', d.month - 1);
-    });
 
     // now plot the data on svg
     heatMapSVG
@@ -203,14 +136,72 @@ d3.json(URL)
       .attr('height', '40px')
       .attr('fill', d => colorScale(d.variance + baseTemperature))
       .attr('x', d => xScale(d.year))
-      .attr('y', d => yScale(new Date(2010, d.month - 1 || 0, 1)))
+      .attr('y', d => yScale(new Date(2010, d.month - 1, 1)))
       .attr('data-month', d =>
-        d3.timeFormat('%B')(new Date(2010, d.month - 1 || 0, 1))
+        d3.timeFormat('%B')(new Date(2010, d.month - 1, 1))
       )
       .attr('data-year', d => d.year)
-      .attr('data-temp', d => d.variance + baseTemperature)
+      .attr('data-temp', d => d.variance)
       .on('mouseover', mouseover)
       .on('mouseleave', mouseleave)
       .attr('transform', d => `translate(2,-20)`);
+
+    // change tick position
+    // d3.selectAll('#y-axis .tick line').forEach(tikcPostion => {
+    //   d3.select(this).attr('transform',`translate(0,)`)
+    // })
+
+    // calculate domain for legend axis
+    //min -6.976 / 1.6840000000000002
+    // max 5.228 / 13.888
+    let tempVariance = dataset.map(d => d.variance + baseTemperature);
+    let tempDelta = (d3.max(tempVariance, d => d) / 10).toFixed(1);
+    let legendDomain = [];
+    for (let i = 0; i <= 10; i++) {
+      legendDomain.push(+(i * tempDelta).toFixed(1));
+    }
+    console.log('legendDomain', legendDomain);
+    console.log('xxxxxxxxxxxxxxxxxxx', d3.format('.0f')(1.2));
+    // console.log('d3.extent(timeDataset)', d3.extent(tempVariance));
+    const legendXScale = d3
+      .scaleLinear()
+      .domain(d3.extent(legendDomain))
+      .range([20, WIDTH - 20]);
+    const legendAxis = d3
+      .axisBottom(legendXScale)
+      .tickSize(13)
+      // .ticks(8)
+      .tickValues([...legendDomain])
+      .tickFormat(d3.format('.2f'));
+    legendSVG.append('g').call(legendAxis).attr('transform', `translate(0,50)`);
+    // .select('.domain')
+    // .remove();
+    d3.select('#legend .tick')
+      .append('g')
+      .selectAll('rects')
+      .data(legendDomain)
+      .enter()
+      .insert('rect', '.tick')
+      .attr('class', 'lCell')
+      .attr('x', (d, i) => {
+        // console.log('dddd', d);
+        return i * ((WIDTH - 20) / 10);
+      })
+      .attr('y', 0)
+      .attr('height', 20)
+      .attr('width', (WIDTH - 20) / 10)
+      .attr('fill', 'red');
+    // create rect
+    // legendSVG
+    //   .selectAll('rects')
+    //   .data([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    //   .enter()
+    //   .append('rect')
+    //   .attr('transform', 'translate(20,50)')
+    //   .attr('class', 'legendClass')
+    //   .attr('width', WIDTH / 12)
+    //   .attr('height', 100)
+    //   .attr('stroke', 'blue')
+    //   .attr('x', (d, i) => (i * WIDTH) / 12);
   })
   .catch(err => console.log(err));
